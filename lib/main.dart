f49aec0296b90407b6da39f91261ade402bc5292
Felix Tse <felixtse283416@gmail.com>
2025-02-20 00:46:51 -0500

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:bday_boost/pages/login_page.dart';
+import 'package:bday_boost/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +17,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: LoginPage(
+        onTap: () {},
+      ),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,101 @@
+import 'package:bday_boost/components/lr_button.dart';
+import 'package:bday_boost/components/lr_textfield.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  const LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // text editing controllers
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+
+            // message, app slogan
+            Text(
+              "Welcome to Bday Boost",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // email textfield
+            LRTextField(
+              controller: emailController,
+              hintText: "Email",
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            // password textfield
+            LRTextField(
+              controller: passwordController,
+              hintText: "Password",
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 25),
+
+            // sign in button
+            LRButton(
+              text: "Sign In",
+              onTap: () {},
+            ),
+
+            const SizedBox(height: 25),
+
+            // not a member? register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not a member?",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                ),
+                const SizedBox(width: 4),
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    "Register now",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
